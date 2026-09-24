cf5cc478faa81f84c8324e5dba324b75ee8dd056
Ankit Kushwah <186614665+Mrankit47@users.noreply.github.com>
2026-09-24 09:01:19 +0530

    ✅ Solve: Two Sum (cpp)

---
+++ Easy/two-sum/two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum =0,maxsum=INT_MIN;
+        for(int num : nums)
+        {
+            sum+=num;
+            maxsum=max(sum,maxsum);
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        return maxsum;
+    }
+};
