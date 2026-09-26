85d41f605558cfba8934f9ff345b6fc978fb52d9
Ankit Kushwah <186614665+Mrankit47@users.noreply.github.com>
2026-09-26 18:07:50 +0530

    ✅ Solve: Not Boring Movies (MySQL)

--- Easy/not-boring-movies/not-boring-movies.sql
+++ Easy/not-boring-movies/not-boring-movies.sql
@@ -1,11 +1,6 @@
-+----------------+----------+
-| Column Name    | Type     |
-+----------------+----------+
-| id             | int      |
-| movie          | varchar  |
-| description    | varchar  |
-| rating         | float    |
-+----------------+----------+
-id is the primary key (column with unique values) for this table.
-Each row contains information about the name of a movie, its genre, and its rating.
-rating is a 2 decimal places float in the range [0, 10]
+# Write your MySQL query statement below
+SELECT *
+FROM Cinema
+WHERE id % 2 = 1
+  AND description <> 'boring'
+ORDER BY rating DESC;
